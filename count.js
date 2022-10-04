671991625d9f4d6e2cbb450967144f5da4874f2c
= <=>
2022-10-04 11:17:15 +0300

    changes in the count function

--- count.js
+++ count.js
@@ -1,5 +1,8 @@
 let counter = 0;
 const count = ()=>{
     counter = counter + 1;
-    alert(counter);
+    counter = document.querySelector('p').innerHTML= counter;
+    if (counter%10===0){
+ alert(`Counter is now ${counter}`);
+    }
 }
