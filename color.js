b7f860fdc63b80e5dc8a4d2a76b87193390ee005
Aloycius Millinga <45605465+aloycius@users.noreply.github.com>
2022-10-12 12:20:49 +0300

    Added color changing files

---
+++ color.js
@@ -0,0 +1,15 @@
+
+document.addEventListener('DOMContentLoaed',function(){
+
+    document.querySelector('#red').onclick = function(){
+document.querySelector('h1').style.color = 'red';
+    }
+
+    document.querySelector('#blue').onclick = function(){
+        document.querySelector('h1').style.color = 'blue';
+            }
+
+            document.querySelector('#green').onclick = function(){
+                document.querySelector('h1').style.color = 'green';
+                    }
+})
