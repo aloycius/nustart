e66bf2b0aba35d5601b2e468d8a6ab4f86564878
Aloycius Millinga <45605465+aloycius@users.noreply.github.com>
2022-10-12 12:53:00 +0300

    Added changes to the h1 element

--- color.js
+++ color.js
@@ -2,14 +2,14 @@
 document.addEventListener('DOMContentLoaed',function(){
 
     document.querySelector('#red').onclick = function(){
-document.querySelector('h1').style.color = 'red';
+document.querySelector('#hello').style.color = 'red';
     }
 
     document.querySelector('#blue').onclick = function(){
-        document.querySelector('h1').style.color = 'blue';
+        document.querySelector('#hello').style.color = 'blue';
             }
 
             document.querySelector('#green').onclick = function(){
-                document.querySelector('h1').style.color = 'green';
+                document.querySelector('#hello').style.color = 'green';
                     }
-})
+});
